ae4c7c63e7963d8cbcb3dc90a874408dbc98c0c0
Danil <danil-8951@mail.ru>
2022-10-23 21:43:06 +0500

    дает сумму цифр в числе

---
+++ Ex027/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Напишите программу, которая принимает на вход 
+число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+
+Console.WriteLine("Введите число : ");
+var number = int.Parse(Console.ReadLine());
+
+var n = number;
+var summ = 0;
+
+while(n != 0)
+{
+    if(n > 0) 
+    {
+        summ = (n / 1000) + (n / 100 % 10) + (n % 100 / 10) + (n % 10);
+        break;
+    }
+}
+
+Console.WriteLine($"Сумма чисел - {summ}");
+
